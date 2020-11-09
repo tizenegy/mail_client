2e7be7ce12dfbf8af0c0004ed9421c2f96bc24e9
Bastian Elfert <bastian.elfert@outlook.com>
2020-11-09 17:30:49 +0200

    implemented fetch (for "sent") and circumvented a bug in models.py with def serialize(): the timestamp regex doesn't seem to work - so I simplified.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -40,12 +40,14 @@ async function send_email(){
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  // get data from api
+  fetch('/emails/sent')
+  .then(response => response.json())
+  .then(emails => console.log(emails));
 }
 
